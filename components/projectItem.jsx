8319de5c74befa8d10f5d67926d26df5c6aaefc8
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-11-18 14:02:24 +0700

    separate item

--- components/projectItem.jsx
+++ components/projectItem.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const ProjectItem = ({title, backgroundImg, url, projectType}) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-3 group hover:bg-gradient-to-r from-blue-600 to-blue-300">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-600 rounded-xl p-3 group hover:bg-gradient-to-r from-blue-600 to-blue-300">
       <Image className="rounded-lg group-hover:opacity-20" src={backgroundImg} alt="Image" width="600" height="400" />
       <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <h3 className="text-2xl text-white tracking-wider text-center">{title}</h3>
---
+++ components/skillItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+const SkillItem = ({name, imageUrl}) => {
+  return (
+    <div className="p-6 shadow-sm bg-white dark:bg-gray-800 rounded-xl hover:scale-105 ease-in duration-300">
+      <div className="grid grid-cols-2 gap-4 justify-center items-center">
+        <div className="m-auto">
+          <Image src={imageUrl} alt="Image" width="64" height="64" />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h3>{name}</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SkillItem
